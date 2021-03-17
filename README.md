2e655e98c5c716b93915d562a2ed18d1b0ed009b
Ben Dubuisson <ben.dubuisson@heyday.co.nz>
2021-03-18 09:11:35 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Useful Azure scripts and Docker images
+
+### SSL renewals
+
+Comes with a [Dockerfile](./ssl-renewal/Dockerfile) so you can just run that image in your pipelines.
+A bit of a very specific case, this allows you to renew Let's Encrypt SSL certs on an Azure App Gateway.
+The App gateway needs to have storage account set up as Backend to receive the certbot challenge (`./well-known/challenge`)
+
+The following environment variables need to be set:
+* `DOMAIN`
+* `MULTI_DOMAIN` ('yes' or 'no', if yes, will do `www` as well)
+* `RESOURCE_GROUP`
+* `GATEWAY_NAME`
+* `GATEWAY_CERT_NAME`
+* `EMAIL` (for certbot comms)
+* `STORAGE_ACCOUNT_NAME`
+* `USERNAME` (service principal)
+* `PASSWORD` (service principal)
+* `TENANT`
+
+Credit mostly to [Isaac](@isaaccarrington) for this one.
---
+++ ssl-renewal/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ${MULTI_DOMAIN} == 'yes' ]; then
+  certbot certonly --manual --manual-auth-hook ${PWD}/auth.sh -d ${DOMAIN} -d www.${DOMAIN} --agree-tos --manual-public-ip-logging-ok --email '${EMAIL}'
+else
+  certbot certonly --manual --manual-auth-hook ${PWD}/auth.sh -d ${DOMAIN} --agree-tos --manual-public-ip-logging-ok --email '${EMAIL}'
+fi
+
+PASSWORD=$(pwgen 29)
+
+# Create PFX file
+openssl pkcs12 -export -out certificate.pfx -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -in /etc/letsencrypt/live/${DOMAIN}/fullchain.pem -password pass:${PASSWORD}
+
+# Update Application Gateway with new SSLCertificate
+az network application-gateway ssl-cert update -g ${RESOURCE_GROUP} --gateway-name ${GATEWAY_NAME} -n ${GATEWAY_CERT_NAME} \
+    --cert-file certificate.pfx --cert-password ${PASSWORD}
