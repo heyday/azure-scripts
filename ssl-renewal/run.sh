8e345272e9a479a771350a98957eae59e6b5ae00
Ryan DJ <ryan.djoenaidi@wundermanthompson.com>
2021-03-24 16:00:11 +1300

    fix conditional syntax

--- ssl-renewal/run.sh
+++ ssl-renewal/run.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-if [[ -z "$MULTI_DOMAIN" || -z "$DOMAIN" || -z "$RESOURCE_GROUP" || -z "$GATEWAY_NAME" || -z "$GATEWAY_CERT_NAME" || -z "$EMAIL" || -z "$STORAGE_ACCOUNT_NAME" || -z "$USERNAME" || -z "$PASSWORD" || -z "$TENANT" ]]; then
-    echo "Missing environment variables" 1>&2
-    exit 1
-fi
+# if [[ -z "$MULTI_DOMAIN" || -z "$DOMAIN" || -z "$RESOURCE_GROUP" || -z "$GATEWAY_NAME" || -z "$GATEWAY_CERT_NAME" || -z "$EMAIL" || -z "$STORAGE_ACCOUNT_NAME" || -z "$USERNAME" || -z "$PASSWORD" || -z "$TENANT" ]]; then
+#     echo "Missing environment variables" 1>&2
+#     exit 1
+# fi
 
 # EXPIRATION_DAY_LEFT is set to 30 if not defined,
 # if this number is larger than number of deys left before cert expire, this process will exit.
 if [[ -z "$EXPIRATION_DAY_LEFT" ]]; then
-    expiration_day_left = 30
+    expiration_day_left=30
 else
-	expiration_day_left = $EXPIRATION_DAY_LEFT
+	expiration_day_left=$EXPIRATION_DAY_LEFT
 fi
 
-date = $(echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -dates | grep "notAfter" | sed "s/.*=\(.*\)/\1/")
-date_s = $(date -d "${date}" +%s)
-now_s = $(date -d now +%s)
-date_diff = $(( (date_s - now_s) / 86400 ))
+website="seaviewproject.co.nz"
+date=$(echo | openssl s_client -servername ${website} -connect ${website}:443 2>/dev/null | openssl x509 -noout -dates | grep "notAfter" | sed "s/.*=\(.*\)/\1/")
+date_s=$(date -d "${date}" +%s)
+now_s=$(date -d now +%s)
+date_diff=$(( (date_s - now_s) / 86400 ))
 
-if date_diff > expiration_day_left then
-	echo "Missing environment variables" 1>&2
+if [ date_diff -gt expiration_day_left ]; then
+	echo "Expiration date is still longer than ${expiration_day_left} day(s)!" 1>&2
 	exit 1
-if
+fi
 
 if [[ ${MULTI_DOMAIN} == 'yes' ]]; then
   certbot certonly --manual --manual-auth-hook ${PWD}/auth.sh -d ${DOMAIN} -d www.${DOMAIN} --agree-tos --manual-public-ip-logging-ok --email ${EMAIL}
