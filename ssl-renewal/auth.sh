b380a2c7aa126fb4b833a9a6187ef348060f8163
Will Rossiter <will.rossiter@akqa.com>
2024-03-15 15:02:26 +1300

    feat: add ADDITIONAL_DOMAINS

--- ssl-renewal/auth.sh
+++ ssl-renewal/auth.sh
@@ -3,8 +3,6 @@
 echo ${CERTBOT_VALIDATION} > ${CERTBOT_TOKEN}
 
 echo "Creating url ${DOMAIN}/.well-known/acme-challenge/${CERTBOT_TOKEN}"
-echo 'file contents is:'
-cat ${CERTBOT_TOKEN}
 
 az login --service-principal --username ${USERNAME} --password ${PASSWORD} --tenant ${TENANT}
 az storage blob upload --account-name ${STORAGE_ACCOUNT_NAME} --container-name \$web --name .well-known/acme-challenge/${CERTBOT_TOKEN} --file ${CERTBOT_TOKEN}
